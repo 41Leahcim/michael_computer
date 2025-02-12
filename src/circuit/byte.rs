481c9afdd51901cb3d5d2c3070afc39830bd2d1a
Michael Scholten <michael-scholten@hotmail.nl>
2025-02-12 20:43:10 +0100

    Added subtraction

--- src/circuit/byte.rs
+++ src/circuit/byte.rs
@@ -25,12 +25,26 @@ pub fn byte_add(left: Byte, right: Byte) -> (Byte, Bit) {
     byte_add_with_carry(left, right, Bit::Low)
 }
 
+/// Subtracts one byte from an other
+pub fn byte_sub(left: Byte, right: Byte) -> (Byte, Bit) {
+    byte_add_with_carry(left, right.not(), Bit::High)
+}
+
+/// Subtracts one byte from an other and subtracts the carry
+pub fn byte_sub_with_carry(mut left: Byte, right: Byte, mut carry: Bit) -> (Byte, Bit) {
+    let mut carry_byte = [Bit::Low; 8];
+    carry_byte[0] = carry;
+    (left, carry) = byte_sub(left, Byte::from(carry_byte));
+    let (result, carry2) = byte_sub(left, right);
+    (result, carry.or(carry2))
+}
+
 #[cfg(test)]
 mod test {
     use crate::{
         bit::Bit,
         byte::Byte,
-        circuit::byte::{byte_add, byte_add_with_carry},
+        circuit::byte::{byte_add, byte_add_with_carry, byte_sub, byte_sub_with_carry},
     };
 
     #[test]
@@ -52,6 +66,30 @@ mod test {
                 assert_eq!(
                     u8::from(byte_add_with_carry(Byte::from(left), Byte::from(right), Bit::High).0),
                     left.wrapping_add(right).wrapping_add(1)
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn subtract() {
+        for left in 0..=u8::MAX {
+            for right in 0..=u8::MAX {
+                assert_eq!(
+                    u8::from(byte_sub(Byte::from(left), Byte::from(right)).0),
+                    left.wrapping_sub(right)
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn subtract_with_carry() {
+        for left in 0..=u8::MAX {
+            for right in 0..=u8::MAX {
+                assert_eq!(
+                    u8::from(byte_sub_with_carry(Byte::from(left), Byte::from(right), Bit::High).0),
+                    left.wrapping_sub(right).wrapping_sub(1)
                 );
             }
         }
